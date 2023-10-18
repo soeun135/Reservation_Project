0f7b338b86abf90d86bc6e7c8283421cdc8cc62d
soeun135 <soensuckun@naver.com>
2023-10-18 21:33:51 +0900

    매장 domain 등록일 필드 추가

--- src/main/java/com/soni/reservation/domain/Store.java
+++ src/main/java/com/soni/reservation/domain/Store.java
@@ -3,13 +3,17 @@ package com.soni.reservation.domain;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "store")
+@EntityListeners(AuditingEntityListener.class)
 public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +25,9 @@ public class Store {
     private String location;
     @NotNull
     private String description;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "MANAGER_ID")
