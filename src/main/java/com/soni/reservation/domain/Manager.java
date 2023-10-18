2acfcfe46e5e4994b10e6b593dc701aa8a112eaf
soeun135 <soensuckun@naver.com>
2023-10-18 21:30:56 +0900

    JpaAuditingConfig 파일 설정

--- src/main/java/com/soni/reservation/domain/Manager.java
+++ src/main/java/com/soni/reservation/domain/Manager.java
@@ -4,6 +4,7 @@ import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDate;
