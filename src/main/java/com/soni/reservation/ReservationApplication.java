45be203506e2f60d8d05645608818ad1a6dfd754
soeun135 <soensuckun@naver.com>
2023-10-26 02:46:59 +0900

    불필요한 import문, 주석 제거

--- src/main/java/com/soni/reservation/ReservationApplication.java
+++ src/main/java/com/soni/reservation/ReservationApplication.java
@@ -1,20 +1,7 @@
 package com.soni.reservation;
 
-import com.soni.reservation.domain.Manager;
-import com.soni.reservation.domain.Store;
-import com.soni.reservation.repository.ManagerRepository;
-import com.soni.reservation.repository.StoreRepository;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.websocket.Session;
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class ReservationApplication {
