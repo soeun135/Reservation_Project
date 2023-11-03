ccb69a90512e203fccb982c429f1d3cf2f7eb865
soeun135 <soensuckun@naver.com>
2023-11-04 03:10:37 +0900

    정책 추가하며 ErrorCode 추가

--- src/main/java/com/soni/reservation/type/ErrorCode.java
+++ src/main/java/com/soni/reservation/type/ErrorCode.java
@@ -21,11 +21,13 @@ public enum ErrorCode {
     RESERVE_NOT_FOUND("예약 건이 없습니다."),
     RESERVE_CANCELED("예약시간 10분 전에 방문하지 않아 예약이 취소되었습니다."),
     RESERVE_NOT_ALLOWED("승인되지 않은 예약 건 입니다."),
+    RESERVE_DUPLICATED("같은 매장에 대해 한 건의 예약만 가능합니다."),
     REVIEW_NOT_ALLOWED("방문하지 않은 예약 건에 대해 리뷰 작성이 불가합니다."),
 
     UNMATCHED_MANAGER_STORE("자신의 매장만 조회할 수 있습니다."),
     UNMATCHED_STORE_RESERVE("해당 매장의 예약건이 아닙니다."),
-    UNMATCHED_MEMBER_RESERVE("해당 회원의 예약건이 아닙니다ㅣ.");
+    UNMATCHED_MEMBER_RESERVE("해당 회원의 예약건이 아닙니다."),
+    UNMATCHED_RESERVE_MANAGER("자신의 매장 예약 건만 승인/거절 할 수 있습니다.");
 
     private final String description;
 }
